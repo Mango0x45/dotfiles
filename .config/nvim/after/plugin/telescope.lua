aa23da0325cba2fa56cb2e68f842543909eac69a
Thomas Voss <mail@thomasvoss.com>
2023-09-30 03:04:30 +0200

    nvim: Call git_files() when in a git repo

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,9 +1,15 @@
+local os = require('os')
 local builtin = require('telescope.builtin')
 local lib = require('mango.lib')
 
---vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
+lib.remap('n', '<leader>pf', function()
+	if os.execute('git rev-parse --is-inside-work-tree') == 0 then
+		builtin.git_files()
+	else
+		builtin.find_files()
+	end
+end)
 
-lib.remap('n', '<leader>pf', builtin.find_files)
 lib.remap('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input('Grep > ') })
 end)
