326a18a91d5b95d6efe397d1e3f3260ad7ecb47f
Thomas Voss <mail@thomasvoss.com>
2023-11-05 21:03:14 +0100

    nvim: Simplify theme config

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -2,22 +2,4 @@ require('rose-pine').setup({
 	disable_background = true
 })
 
-function SetTheme(color)
-	local hl = vim.api.nvim_set_hl
-	vim.cmd.colorscheme(color or 'rose-pine')
-	vim.g.neovide_transparency = 0.9
-	hl(0, 'Normal', vim.g.neovide and {
-		fg = '#C5C8C6',
-		bg = '#2B303B',
-	} or {
-		fg = 'none',
-		bg = 'none',
-	})
-
-	hl(0, 'NormalFloat', { bg = 'none' })
-	hl(0, 'LineNr', { fg = 'white' })
-	hl(0, 'LineNrAbove', { fg = 'grey' })
-	hl(0, 'LineNrBelow', { fg = 'grey' })
-end
-
-SetTheme()
+vim.cmd.colorscheme('rose-pine')
