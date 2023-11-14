5b7b0f77d8a37e7cfd5ddd901f5bcd295413a0bf
Thomas Voss <mail@thomasvoss.com>
2023-11-14 22:30:05 +0100

    nvim: Remove unused code

--- .config/nvim/lua/mango/packer.lua
+++ .config/nvim/lua/mango/packer.lua
@@ -53,21 +53,4 @@ return require('packer').startup(function(use)
 			{ 'saadparwaiz1/cmp_luasnip' },
 		},
 	}
-
-	-- LSP support
-	-- use {
-	-- 	'VonHeikemen/lsp-zero.nvim',
-	-- 	branch = 'v2.x',
-	-- 	requires = {
-	-- 		-- LSP Support
-	-- 		{ 'neovim/nvim-lspconfig' },
-	-- 		{ 'williamboman/mason.nvim' },
-	-- 		{ 'williamboman/mason-lspconfig.nvim' },
-
-	-- 		-- Autocompletion
-	-- 		{ 'hrsh7th/nvim-cmp' },
-	-- 		{ 'hrsh7th/cmp-nvim-lsp' },
-	-- 		{ 'L3MON4D3/LuaSnip' },
-	-- 	}
-	-- }
 end)
