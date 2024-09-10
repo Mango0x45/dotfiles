b9365b662d056f47fe2b821b120120e07430679b
Thomas Voss <mail@thomasvoss.com>
2024-09-10 21:00:40 +0200

    emacs: Remove big comment

--- .config/emacs/mango-theme.el
+++ .config/emacs/mango-theme.el
@@ -1,30 +1,5 @@
 ;;; mango-theme.el --- Just your average dark theme  -*- lexical-binding: t; -*-
 
-;; Copyright © 2023–2024 Thomas Voss
-
-;; Author: Thomas Voss <mail@thomasvoss.com>
-;; Maintainer: Thomas Voss <mail@thomasvoss.com>
-;; URL: https://git.thomasvoss.com/dotfiles
-
-;;; License:
-
-;; Permission to use, copy, modify, and/or distribute this software for any
-;; purpose with or without fee is hereby granted.
-;;
-;; The software is provided ‘as is’ and the author disclaims all warranties with
-;; regard to this software including all implied warranties of merchantability
-;; and fitness.  In no event shall the author be liable for any special, direct,
-;; indirect, or consequential damages or any damages whatsoever resulting from
-;; loss of use, data or profits, whether in an action of contract, negligence or
-;; other tortious action, arising out of or in connection with the use or
-;; performance of this software.
-
-;;; Commentary:
-
-;; TODO
-
-;;; Code:
-
 (deftheme mango
   "Just another dark theme because none of the other options out there were just
 as I would like them.  Why try to fix someone elses themes when I make my own?")
