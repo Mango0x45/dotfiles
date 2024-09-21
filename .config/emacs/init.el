15ac54bd5a1683e398f0e566ebcbb7aa88c04166
Thomas Voss <mail@thomasvoss.com>
2024-09-21 07:46:45 +0200

    emacs: Add dired binding

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -871,6 +871,11 @@ the comparison of the ‘company-pseudo-tooltip-overlay’ height and 0 using PR
     ("a" eglot-code-actions)
     ("r" eglot-rename)))
 
+(use-package dired
+  :ensure nil
+  :config
+  (evil-define-key 'normal 'dired-mode-map "n" #'find-file))
+
 ;;; Context-Specific Modes
 (dolist (mode '(c-mode c-ts-mode go-ts-mode))
   (add-hook (x-mode-to-hook mode) #'electric-pair-local-mode)
