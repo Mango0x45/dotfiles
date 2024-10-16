8fe67d640e96d58c32cf0bd6168937a3986502b7
Thomas Voss <mail@thomasvoss.com>
2024-10-16 23:12:52 +0200

    emacs: Discover that ‘calc-roll-down’ exists

--- .config/emacs/modules/mm-calc.el
+++ .config/emacs/modules/mm-calc.el
@@ -1,18 +1,6 @@
 ;;; mm-calc.el --- Emacs configurations for ‘calc-mode’  -*- lexical-binding: t; -*-
 
-;; TODO: Swap more than 2 elements?
-(defun mm-calc-swap ()
-  "Swap the top two elements on the stack."
-  (declare (modes calc-mode))
-  (interactive)
-  (calc-over 2)
-  (calc-truncate-up 2)
-  (calc-pop 1)
-  (calc-truncate-down 2))
-
 (use-package calc
-  :bind (:map calc-mode-map
-         ("C-c x" . #'mm-calc-swap))
   ;; TODO: Can this be done in :custom?
   :init
   (setopt
