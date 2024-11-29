025b5fc21b25c8d8ec4c08619576fe56889e25bc
Thomas Voss <mail@thomasvoss.com>
2024-11-29 09:20:25 +0100

    emacs: Be recursive in dired and set ls(1) switches

--- .config/emacs/modules/mm-dired.el
+++ .config/emacs/modules/mm-dired.el
@@ -3,7 +3,13 @@
 (use-package dired
   :hook ((dired-mode . dired-omit-mode)
          (dired-mode . dired-hide-details-mode))
-  :bind (:map dired-mode-map
-         ("f" . find-file)))
+  :bind ( :map dired-mode-map
+          ("f" . find-file))
+  :custom
+  (dired-recursive-copies 'always)
+  (dired-recursive-deletes 'always)
+  (dired-listing-switches
+   (combine-and-quote-strings
+    '("-AFGhlv" "--group-directories-first" "--time-style=+%d %b %Y %T"))))
 
 (provide 'mm-dired)
