6eac80b022faea4ab399afbf04d8ee19115baf18
Thomas Voss <mail@thomasvoss.com>
2025-11-05 20:33:23 +0100

    emacs: Add cmake-mode

--- .config/emacs/modules/mm-editing.el
+++ .config/emacs/modules/mm-editing.el
@@ -373,6 +373,7 @@ is as described by `emmet-expand-line'."
 ;;; Additional Major Modes
 
 (use-package awk-ts-mode :ensure t)
+(use-package cmake-mode  :ensure t)
 (use-package git-modes   :ensure t)
 (use-package po-mode     :ensure t)
 (use-package sed-mode    :ensure t)
