50028601dfff80e372b187a9fc4cd8e47f35bd60
Thomas Voss <mail@thomasvoss.com>
2026-03-08 21:14:43 +0100

    emacs: Install kdl-mode

--- .config/emacs/modules/mm-editing.el
+++ .config/emacs/modules/mm-editing.el
@@ -381,6 +381,7 @@ is as described by `emmet-expand-line'."
 (use-package awk-ts-mode :ensure t)
 (use-package cmake-mode  :ensure t)
 (use-package git-modes   :ensure t)
+(use-package kdl-mode    :ensure t)
 (use-package po-mode     :ensure t)
 (use-package sed-mode    :ensure t)
 
