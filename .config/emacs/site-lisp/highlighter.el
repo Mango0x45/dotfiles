ca98d830d5633cec786e5860aa5a46848bac2429
Thomas Voss <mail@thomasvoss.com>
2026-04-05 01:47:59 +0200

    emacs: Multiple cursors support for highlighter.el

--- .config/emacs/site-lisp/highlighter.el
+++ .config/emacs/site-lisp/highlighter.el
@@ -1,20 +1,92 @@
 ;;; highlighter.el --- In-buffer highlighting commands  -*- lexical-binding: t; -*-
 
-;; TODO: Support multiple cursors
+(require 'seq)
 
-(eval-when-compile
-  (require 'seq))
+;;; Public Options
 
 (defgroup highlighter nil
   "Customization group for `highlighter'."
-  :group 'convenience)
+  :group 'convenience
+  :prefix "highlighter-")
 
-(defcustom highlighter-default-face 'match
-  "The default face used by `highlighter-mark'."
+(defcustom highlighter-default-face nil
+  "The default face used to highlight regions.
+If `nil' the `match' face is used."
   :type 'face
   :package-version '(highlighter . "1.0.0")
   :group 'highlighter)
 
+(defcustom highlighter-multiple-cursors-prompt-once-p t
+  "If non-nil, only prompt for a face once when using `multiple-cursors'.
+When using the `multiple-cursors' package, commands that prompt the
+user for a face will prompt the user for every single cursor.  When
+set to a non-nil value this option ensures that the user is only
+prompted once."
+  :type 'boolean
+  :package-version '(highlighter . "1.0.0")
+  :group 'highlighter)
+
+
+;;; Private Helpers
+
+(defun highlighter--mark-region (beg end &optional face)
+  (let ((ov (make-overlay beg end nil :front-advance))
+        (face (or face highlighter-default-face 'match)))
+    (overlay-put ov 'priority 1)
+    (overlay-put ov 'face face)
+    (overlay-put ov 'evaporate t)
+    (overlay-put ov 'highlighter--mark-p t)
+    (overlay-put ov 'highlighter--face face)))
+
+(defun highlighter--unmark-region (beg end &optional face)
+  (if face
+      (remove-overlays beg end 'highlighter--face face)
+    (remove-overlays beg end 'highlighter--mark-p t)))
+
+(defvar-local highlighter--selected-face nil)
+
+(defun highlighter--read-face-name (prompt face-predicate)
+  (if (and (bound-and-true-p mc--executing-command-for-fake-cursor)
+           highlighter-multiple-cursors-prompt-once-p)
+      highlighter--selected-face
+    (setq highlighter--selected-face nil)
+    (let ((prompt (format "%s: " prompt))
+          (completion-extra-properties
+           `(:affixation-function
+             ,(lambda (faces)
+                (mapcar
+                 (lambda (face)
+                   (list face
+                         (concat (propertize read-face-name-sample-text
+                                             'face face)
+                                 "\t")
+                         ""))
+                 faces))))
+          aliasfaces nonaliasfaces)
+      ;; Build up the completion tables.
+      (mapatoms (lambda (s)
+                  (when (apply face-predicate s nil)
+                    (if (get s 'face-alias)
+                        (push (symbol-name s) aliasfaces)
+                      (push (symbol-name s) nonaliasfaces)))))
+      (let ((face (completing-read
+                   prompt
+                   (completion-table-in-turn nonaliasfaces aliasfaces)
+                   nil t nil 'face-name-history)))
+        (when (facep face)
+          (setq highlighter--selected-face
+                (if (stringp face)
+                    (intern face)
+                  face)))))))
+
+(defun highlighter--used-face-p (face)
+  (seq-filter (lambda (ov) (eq face (overlay-get ov 'highlighter--face)))
+              (overlays-in (point-min) (point-max))))
+
+
+;;; Public API
+
+;;;###autoload
 (defun highlighter-mark (arg)
   "Highlight text in the buffer.
 Highlight the current line or region if it is active.  Text is
@@ -32,6 +104,7 @@ With ARG, interactively pick a face to highlight with."
   (when (region-active-p)
     (deactivate-mark)))
 
+;;;###autoload
 (defun highlighter-unmark (arg)
   "Remove highlights in the buffer.
 
@@ -52,37 +125,7 @@ face will be removed."
   (when (region-active-p)
     (deactivate-mark)))
 
-(defun highlighter-mark-region (bounds &optional face)
-  "Highlight text in the buffer within BOUNDS.
-BOUNDS uses the same format as returned by `region-bounds'.
-
-Text is highlighted using the face specified by
-`highlighter-default-face'.
-
-If FACE is nil or omitted, `highlighter-default-face' is used."
-  (dolist (x bounds) (highlighter--mark-region (car x) (cdr x) face)))
-
-(defun highlighter-unmark-region (bounds &optional face)
-  "Remove highlights in the buffer within BOUNDS.
-BOUNDS uses the same format as returned by `region-bounds'.
-
-If FACE is non-nil, only remove highlights using FACE."
-  (dolist (x bounds) (highlighter--unmark-region (car x) (cdr x) face)))
-
-(defun highlighter--mark-region (beg end &optional face)
-  (let ((ov (make-overlay beg end nil :front-advance))
-        (face (or face highlighter-default-face 'match)))
-    (overlay-put ov 'priority 1)
-    (overlay-put ov 'face face)
-    (overlay-put ov 'evaporate t)
-    (overlay-put ov 'highlighter--mark-p t)
-    (overlay-put ov 'highlighter--face face)))
-
-(defun highlighter--unmark-region (beg end &optional face)
-  (if face
-      (remove-overlays beg end 'highlighter--face face)
-    (remove-overlays beg end 'highlighter--mark-p t)))
-
+;;;###autoload
 (defun highlighter-unmark-buffer (arg)
   "Remove highlights in the buffer.
 
@@ -96,37 +139,20 @@ face will be removed."
                  #'highlighter--used-face-p))))
     (highlighter--unmark-region (point-min) (point-max) face)))
 
-(defun highlighter--read-face-name (prompt face-predicate)
-  (let (default defaults)
-    (let ((prompt (format "%s: " prompt))
-          (completion-extra-properties
-           `(:affixation-function
-             ,(lambda (faces)
-                (mapcar
-                 (lambda (face)
-                   (list face
-                         (concat (propertize read-face-name-sample-text
-                                             'face face)
-                                 "\t")
-                         ""))
-                 faces))))
-          aliasfaces nonaliasfaces faces)
-      ;; Build up the completion tables.
-      (mapatoms (lambda (s)
-                  (when (apply face-predicate s nil)
-                    (if (get s 'face-alias)
-                        (push (symbol-name s) aliasfaces)
-                      (push (symbol-name s) nonaliasfaces)))))
-      (let ((face (completing-read
-                   prompt
-                   (completion-table-in-turn nonaliasfaces aliasfaces)
-                   nil t nil 'face-name-history defaults)))
-        (when (facep face) (if (stringp face)
-                               (intern face)
-                             face))))))
+;;;###autoload
+(defun highlighter-mark-region (bounds &optional face)
+  "Highlight text in the buffer within BOUNDS using FACE.
+BOUNDS uses the same format as returned by `region-bounds'.
 
-(defun highlighter--used-face-p (face)
-  (seq-filter (lambda (ov) (eq face (overlay-get ov 'highlighter--face)))
-              (overlays-in (point-min) (point-max))))
+If FACE is nil or omitted, `highlighter-default-face' is used."
+  (dolist (x bounds) (highlighter--mark-region (car x) (cdr x) face)))
+
+;;;###autoload
+(defun highlighter-unmark-region (bounds &optional face)
+  "Remove highlights in the buffer within BOUNDS.
+BOUNDS uses the same format as returned by `region-bounds'.
+
+If FACE is non-nil, only remove highlights using FACE."
+  (dolist (x bounds) (highlighter--unmark-region (car x) (cdr x) face)))
 
 (provide 'highlighter)
